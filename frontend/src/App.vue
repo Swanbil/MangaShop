c8353a4ee0d677a2d8bf5aa93953f6f4b5664b75
Swan <swan.bilek@efrei.net>
2021-09-21 21:17:27 +0200

    Add structure frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="test()">TEST</button>
+    {{value}}
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      value:'rien'
+    };
+  },
+  methods:{
+    async test(){
+      const response = await axios.get('/api/test');
+      console.log(response.data)
+      this.value = response.data.message
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
