89ea21424b7c34d6475f264df0ac49b3a5c1a52a
Swan <swan.bilek@efrei.net>
2021-09-22 20:04:40 +0200

    Footer and colors

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="header p-1">
     <ul class="d-flex flex-row p-1 align-items-center">
-      <li><router-link class="link" to="/">Home</router-link></li>
-      <li>
-        <router-link class="link" to="/login">Product</router-link>
-      </li>
+      <router-link class="link" to="/"><li>Home</li></router-link>
+      <router-link class="link" to="/login"><li>Product</li></router-link>
       <li class="ms-auto p-2">
         <router-link class="link" to="/login">
           <b-icon icon="person-circle" aria-hidden="true"></b-icon>
@@ -22,23 +20,22 @@ export default {
 </script>
 
 <style scoped>
-.header{
-    background-color:rgb(245, 245, 212);
+.header {
+  background-color: rgb(250, 250, 229);
 }
 li {
   list-style: none;
   font-size: 20px;
   font-weight: bold;
-  padding:10px;
-  
+  padding: 10px;
 }
-li:hover{
-    background-color:rgb(235, 235, 235);
+li:hover {
+  background-color: rgb(235, 235, 235);
 }
 .link {
   text-decoration: none;
   font-size: 20px;
   font-weight: bold;
-  color:#a59a8d;
+  color: #a59a8d;
 }
 </style>
